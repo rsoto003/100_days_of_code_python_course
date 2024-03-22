2aa56ddf7e35daddf0d02a3357bc7e57ff6b6e3f
Ryan Soto <ryansotodev@gmail.com>
2024-03-21 23:13:35 -0700

    input function intro

--- section_1_day_1/main.py
+++ section_1_day_1/main.py
@@ -1,11 +1,14 @@
-print('hello world')
-
 # lesson 1 day 1 - printing
-print('1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.')
-print('2. Knead the dough for 10 minutes.')
-print('3. Add 3g of Salt.')
-print('4. Leave to rise for 2 hours.')
-print('5. Bake at 200 degrees C for 30 minutes.')
+# print('1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.')
+# print('2. Knead the dough for 10 minutes.')
+# print('3. Add 3g of Salt.')
+# print('4. Leave to rise for 2 hours.')
+# print('5. Bake at 200 degrees C for 30 minutes.')
 
-print('She said: "Hello" and then left.') #quote within a string example.
-print("She said: \"Hello\" and then left.") #using escape characters within a string.
+# print('She said: "Hello" and then left.') #quote within a string example.
+# print("She said: \"Hello\" and then left.") #using escape characters within a string.
+
+# lesson 1 day 1 - input function
+
+# input prompt gets user input, value is then concatenated on to the print statement.
+print("Hello " + input("What is your name?") + "!")
